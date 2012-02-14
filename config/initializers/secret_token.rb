2455cba3346eedfb44243b2724556b6f40cd7a5c
Trenton Johnson <trenton.johnson@sv.cmu.edu>
2012-02-14 17:10:21 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileProject::Application.config.secret_token = 'efd710dc46d2a7a6660bc3dd92dc7a5c1c9d9161702ecd57527e774df1d8407b4b40fabf1fe923790ed0cfce185a4b081b51fd782975b51bf4a34b153ca869b3'
